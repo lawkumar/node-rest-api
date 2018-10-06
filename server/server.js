159be35da3f701b24cf8ea54c4cb4819c55187b7
lawkumar <lawkumar9@gmail.com>
2018-10-07 01:00:57 +0530

    Add GET: /todos/id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var {ObjectID} = require('mongodb'); 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -22,7 +22,7 @@ todo.save().then((doc) => {
 }) ;
 });
 
-// GET /todos
+// GET /todos   
 
 app.get('/todos', (req, res) => {
   Todo.find().then((todo)=>{
@@ -31,6 +31,28 @@ app.get('/todos', (req, res) => {
     res.status(400).send(error);
   })
 });
+
+//GET /todos/id
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+   if (!ObjectID.isValid(id))
+   {
+     return res.status(404).send();
+   }
+
+  Todo.findById(id).then((todo) => {
+     if (!todo) 
+     {
+       return res.status(404).send();
+     }
+     return res.status(200).send({todo});
+  }, (err) => {
+     return res.status(400).send();
+  })
+});
+
+
 
 
 
